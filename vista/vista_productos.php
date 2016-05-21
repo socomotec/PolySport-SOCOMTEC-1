bdca81c1f85728d7e89849e038e310e23ac9a8a6
misaelo3 <misael.es.ar.or@gmail.com>
2016-05-21 19:40:12 -0400

    aaa

--- vista/vista_productos.php
+++ vista/vista_productos.php
@@ -29,10 +29,41 @@ $Id_categoria = $_REQUEST['id_categoria'];
 		<script src="js/miEstilo.js"></script>
 		
 		<script>
-			function mostrar() {
+			
+			var nombre, marca, precio, descripcion, img;
+			
+			function mostrar(){
+				
+				limpiar();
+				
+				nombre = $("#nombre").text();
+				marca = $("#marca").text();
+				precio = $("#precio").text();
+				descripcion =$("#descripcion").text();
+				img = $("#imagen-producto").attr("src");
+				
+				$("#Mmarca").html("<strong>Marca: </strong>" + marca);
+				$("#Mprecio").html("<strong>precio: </strong>" + precio);
+				$("#Mdescripcion").html("<strong>Descripcion: </strong>" + descripcion);
+				$("#Mproducto").html("<strong>Nombre: </strong>" + nombre);
+				$("#MimgNormal").attr("src",img);
+				$("#MimgNormal").addClass("imagen-producto");
 				$('#mostrar').modal('show');
 			}
-			    		
+			
+			function limpiar(){
+				nombre = "";
+				marca = "";
+				precio = "";
+				descripcion = "";
+				img = "";
+				
+				$("#Mmarca").html("");
+				$("#Mprecio").html("");
+				$("#Mdescripcion").html("");
+				$("#Mproducto").html("");
+				$("#MimgNormal").attr("src", null);
+			}    		
 			/*$("#ver-"+arre[i]).click(function(event) {
 				var tribut = $(this).attr("id"); //EXTREMOS EL NUMERO DE LA ID
          		var formudato = new FormData();
@@ -70,21 +101,19 @@ $Id_categoria = $_REQUEST['id_categoria'];
 			<hr />	
 </div>
 
-<div class="container">
+	<div class="container">
 			<?php foreach (producto_categoria($Id_categoria) as $datos) {?>
 			<!-- modificar para crear cuadros -->	
-				<div class="container-fluid col-md-4" onclick="mostrar()">
-						<div class="zoom">
-							<div class="thumbnail">
-								<img src=<?php echo $datos["url_producto"].$datos["nombre_img"];?> id="imagen-producto"/>
-								<div class="texto">
-									<h2 style="text-align:center;"><strong><?php echo $datos["nombre_producto"]; ?></strong></h2>
-									<p><strong>Marca:</strong> <?php echo $datos["nombre_marca"]; ?> </p>
-									<p><strong>Precio:</strong> $<?php echo $datos["precio"]; ?> </p>
-									<P class="ocultartxt"><strong>Descripcion: </strong><?php echo $datos["descripcion"]; ?></P>
-								</div>
-							</div>
+				<div  class="container-fluid col-xs-12 col-sm-6 col-md-4" onclick="mostrar()">
+					<div class="zoom">
+						<div class="thumbnail">
+							<img  id="imagen-producto" src=<?php echo $datos["url_producto"].$datos["nombre_img"];?> class="img-responsive" alt="Responsive image"/>
+							<h2  id="nombre" class="corre" style="text-align:center;"><strong><?php echo $datos["nombre_producto"]; ?></strong></h2>
+							<p  id="marca" class="corre"><strong>Marca:</strong> <?php echo $datos["nombre_marca"]; ?> </p>
+							<p  id="precio"class="corre"><strong>Precio:</strong> $<?php echo $datos["precio"]; ?> </p>
+							<P  id="descripcion"class="ocultartxt corre"><strong>Descripcion: </strong><?php echo $datos["descripcion"]; ?></P>					
 						</div>
+					</div>
 				</div>
 			<?php } ?>
 		</div> 
@@ -102,23 +131,29 @@ $Id_categoria = $_REQUEST['id_categoria'];
 	precio
 	la descripcion
 	--->
-	<div id="mostrar" class="modal">
-		<div class="modal-dialog">
-			<div class="modal-content">
+<div id="mostrar" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title">Modal title</h4>
+				<h4 id="Mproducto" class="modal-title">Modal title</h4>
 			</div>
 			<div class="modal-body">
-				<p>One fine body…</p>
+				<div class="row col-sm-8">
+					<img id="MimgNormal"/>
+				</div>
+				<div class="row col-sm-4">
+					<p id="Mmarca"></p>
+					<p id="Mprecio"></p>
+					<P id="Mdescripcion"></P>	
+				</div>
 			</div>
 			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary">Save changes</button>
-			</div>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
 			</div>
 		</div>
 	</div>
+</div>
   
 </body>
 
